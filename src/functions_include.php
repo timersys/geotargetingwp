20302b655136573c61abf34bf2755e4c3c07f14a
Damian Logghe <damian@timersys.com>
2017-04-10 19:53:09 -0300

    added dontenv

--- src/functions_include.php
+++ src/functions_include.php
@@ -1,4 +1,9 @@
 <?php
+use Dotenv\Dotenv;
+// load end files
+$dotenv = new Dotenv(dirname(__DIR__));
+$dotenv->load();
+
 // Don't redefine the functions if included multiple times.
 if ( !function_exists('GeotargetingWP\get_user_ip') ) {
 	require __DIR__ . '/functions.php';
