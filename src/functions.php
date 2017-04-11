05ad3a71b8eb711dda8e4b9b81796f21daa6eef5
Damian Logghe <damian@timersys.com>
2017-04-11 19:02:14 -0300

    debug function

--- src/functions.php
+++ src/functions.php
@@ -71,3 +71,25 @@ function env($key, $default = null) {
 	return $value;
 
 }
+
+/**
+ * Debug function
+ * @return string
+ */
+function generateCallTrace() {
+	$e = new \Exception();
+	$trace = explode("\n", $e->getTraceAsString());
+	// reverse array to make steps line up chronologically
+	$trace = array_reverse($trace);
+	array_shift($trace); // remove {main}
+	array_pop($trace); // remove call to this method
+	$length = count($trace);
+	$result = array();
+
+	for ($i = 0; $i < $length; $i++)
+	{
+		$result[] = ($i + 1)  . ')' . substr($trace[$i], strpos($trace[$i], ' ')); // replace '#someNum' with '$i)', set the right ordering
+	}
+
+	return "\t" . implode("\n\t", $result);
+}
